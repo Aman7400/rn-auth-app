5eeceb38900f8375a371b27969e821fa76e48919
Aman Shukla <amanks7400@gmail.com>
2022-07-25 14:19:03 +0530

    Update Home.js

--- src/screens/main/Home.js
+++ src/screens/main/Home.js
@@ -1,34 +1,40 @@
-import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { Button, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { AuthContext } from '../../contexts/AuthContext'
-import { Button as PaperButton } from 'react-native-paper';
+import { Button as PaperButton, Avatar as ProfileAvatar } from 'react-native-paper';
 
-const Home = ({navigation}) => {
-    const {logout,userProfile} = React.useContext(AuthContext)
-  return (
-    <SafeAreaView>
-        <View style={{
-            padding:16
-        }}>
 
-            {/* Top */}
-            <View style={{flexDirection:'row'}}>
-                <Text style={{
-                    fontSize:18
-                }}>Hello</Text>
-                <Text style={{
-                    color: 'blue',
-                    fontWeight: 'bold',
-                    fontSize: 18,
-                }}>,{userProfile.fullName}</Text> 
+const Home = ({ navigation }) => {
+    const { logout, userProfile } = React.useContext(AuthContext)
+    return (
+        <SafeAreaView>
+            <View style={{
+                padding: 18
+            }}>
+
+                {/* Top */}
+                <View style={{ flexDirection: 'row', justifyContent: "space-between",alignItems:"center",marginBottom:16 }} >
+                    <View style={{ flexDirection: 'row' }}>
+                        <Text style={{
+                            fontSize: 24
+                        }}>Hello</Text>
+                        <Text style={{
+                            color: 'blue',
+                            fontWeight: 'bold',
+                            fontSize: 24,
+                        }}>, {userProfile.fullName}</Text>
+                    </View>
+
+                    <TouchableOpacity onPress={() => navigation.navigate("Settings")}>
+                        <ProfileAvatar.Image size={48} source={{uri: userProfile.profilePic || 'https://john-mohamed.com/wp-content/uploads/2018/05/Profile_avatar_placeholder_large.png'}} />
+                    </TouchableOpacity>
+                </View>
+                <PaperButton icon="logout" mode="contained" onPress={() => logout()}>
+                    Log out
+                </PaperButton>
             </View>
-            <Button title=""  />
-            <PaperButton icon="logout" mode="contained" onPress={() => logout()}>
-            Log out
-  </PaperButton>
-        </View>
-    </SafeAreaView>
-  )
+        </SafeAreaView>
+    )
 }
 
 export default Home
